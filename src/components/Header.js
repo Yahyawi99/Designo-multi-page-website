c7396cadc90457370520ae9caef6ca951547bc90
Yassin Yahyawi <96199527+Yahyawi99@users.noreply.github.com>
2022-07-31 13:46:48 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="/assets/shared/desktop/logo-dark.png" alt="logo" />
+
+      <ul>
+        <li>OUR COMPANY</li>
+        <li>LOACTIONS</li>
+        <li>CONTACT</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+// components
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <section className="home">
+      <Header />
+    </section>
+  );
+};
+
+export default Home;
